0ca42098d528cf5fc4f21cea998c8bd123769104
Deepikaik <deepikabhagavati2001@gmail.com>
2024-08-04 00:32:27 +0530

    'a'

--- Assignmentss/iterative/40.js
+++ Assignmentss/iterative/40.js
@@ -1,1 +1,6 @@
 //40. Write a program to print 5 to 15 numbers using while Loop
+let i=5;
+while (i<=15){
+    console.log(i)
+    i++
+}
---
+++ Assignmentss/iterative/53.js
@@ -0,0 +1,7 @@
+//53. Write a program to print 15 to 10 numbers using do while Loop
+let i=15;
+do{
+    console.log(i)
+    i--
+}
+while(i>=10)
